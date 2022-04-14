a2bf9b33c39e53096aee1e5183507fd551cb6943
Cordell <cordellbonnieux@gmail.com>
2022-04-13 21:01:15 -0700

    First commit, mostly set up firebase backend.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React from 'react'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { usecollectionData } from 'react-firebase-hooks/firestore'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBQLP75avSXbBL1ulKUmFDnSHvsLXVFCEU",
+  authDomain: "chatapp-60ec8.firebaseapp.com",
+  projectId: "chatapp-60ec8",
+  storageBucket: "chatapp-60ec8.appspot.com",
+  messagingSenderId: "297251340167",
+  appId: "1:297251340167:web:2c6e56c3bdc7eceffadd22",
+  measurementId: "G-61P4VE8G99"
+})
+
+const auth = firebase.aurth()
+const firestore = firebase.firestore()
 
 function App() {
+
+  const [user] = useAuthState(auth)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+      <section>
+        { user ? <ChatRoom /> : <SignIn />}
+      </section>
     </div>
   );
 }
 
-export default App;
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleauthProvider()
+  }
+
+  return (
+    <button onClick={signInWithGoogle}>Sign In With Google</button>
+  )
+}
+
+export default App
