80ff55c694c4bcdbfba01891c918bb20a7ce63b9
Cordell <cordellbonnieux@gmail.com>
2022-04-15 11:30:51 -0700

    Updated readme

--- functions/index.js
+++ functions/index.js
@@ -13,15 +13,22 @@ const Filter = require('bad-words');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-const db = admin.firestore()
+const db = admin.firestore();
 
-exports.detectEvilUsers = functions.firestore.document('messages/{msgId').onCreate(async (doc, ctx) => {
+/*
+on create not accepting a function as param
+
+
+exports.detectEvilUsers = functions.firestore.document(`messages/{msgId}`).onCreate(create(doc, ctx));
+
+async function create(doc, ctx) {
     const filter = new filter();
-    const { text, uid } = doc.data()
+    const { text, uid } = doc.data();
 
     if (filter.isProfane(text)) {
         const cleaned = filter.clean(text);
         await doc.ref.update({text: 'I got banned for saying naughty words'});
         await db.collection('banned').doc(uid).set({});
     }
-});
+}
+*/
